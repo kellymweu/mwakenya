98b72c8fc26b64d5aa4f63f26ce12eac9a6d92d2
kellymweu <mweukelly@gmail.com>
2024-07-12 21:28:40 +0300

    implements new carousel

--- frontend/components/Carousel.jsx
+++ frontend/components/Carousel.jsx
@@ -35,7 +35,7 @@ const Carousel = () => {
   }, [activeImage]);
 
   return (
-    <div className="h-[60vh] w-[200vh] mt-10 grid place-items-center mx-auto max-w-xl shadow-2xl rounded-l">
+    <div className="h-[60vh] w-[200vh] mt-10 grid place-items-center mx-auto max-w-xl rounded-md">
       <div className=" w-full flex justify-center items-center gap-0 transition-transform ease-in-out duration-500 rounded-xl ">
         {/* map through images which are located have been stored in our constants */}
         {images.map((pic, idx) => (
@@ -52,7 +52,7 @@ const Carousel = () => {
               alt=""
               width={400}
               height={400}
-              className="w-full h-[50vh] object-cover rounded-tl-xl rounded-bl-xl hover:shadow-2xl hover:cursor-pointer"
+              className="w-full h-[50vh] object-cover rounded-l hover:shadow-2xl hover:cursor-pointer"
             />
           </div>
         ))}
